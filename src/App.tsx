e2f499b5e1d9525238c16fc29acf5ebf79b72783
JFour404 <justas00001@gmail.com>
2026-02-07 23:17:33 +0200

    Project structure

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import "./styles/index.css";
-import { WorkspaceLayout } from "./components/WorkspaceLayout";
-
-export function App() {
-  return <WorkspaceLayout />;
-}
-
-export default App;
---
+++ src/app/App.tsx
@@ -0,0 +1,8 @@
+import "../styles/index.css";
+import { WorkspaceLayout } from "../components/WorkspaceLayout";
+
+export function App() {
+  return <WorkspaceLayout />;
+}
+
+export default App;
