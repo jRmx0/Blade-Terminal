5bf426e4eff8f03262186e3454feac53c8eb9126
JFour404 <justas00001@gmail.com>
2026-02-12 23:16:41 +0200

    feat(workspace): placeholder buttons

---
+++ src/features/workspace-manager/components/WorkspaceNameField/WorkspaceNameField.tsx
@@ -0,0 +1,9 @@
+export default function WorkspaceNameField() {
+  return (
+    <input
+      type="text"
+      placeholder="Filename"
+      className="w-full px-2 py-1 text-sm text-gray-700 bg-white rounded outline-none"
+    />
+  );
+}
