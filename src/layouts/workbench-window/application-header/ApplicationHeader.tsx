9fcc72f973bbbdcf3600a4bc1fa1a8cf538336df
JFour404 <justas00001@gmail.com>
2026-02-13 20:10:39 +0200

    style(application-header): reduce vertical padding

--- src/layouts/workbench-window/application-header/ApplicationHeader.tsx
+++ src/layouts/workbench-window/application-header/ApplicationHeader.tsx
@@ -4,7 +4,7 @@ import MenuBar from "./menu-bar/MenuBar";
 
 export default function ApplicationHeader() {
   return (
-    <div className="flex items-center gap-3 px-3 py-2 bg-gray-100 border border-gray-300">
+    <div className="flex items-center gap-3 px-3 py-1 bg-gray-100 border border-gray-300">
       <div className="shrink-0">
         <AppLogoBox />
       </div>
