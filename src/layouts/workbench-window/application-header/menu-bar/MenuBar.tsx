c0d3abc69242a02d8d17cbf0f7b4687294931c80
JFour404 <justas00001@gmail.com>
2026-02-13 20:30:10 +0200

    refactor(menu-bar): list renamed to submenu

--- src/layouts/workbench-window/application-header/menu-bar/MenuBar.tsx
+++ src/layouts/workbench-window/application-header/menu-bar/MenuBar.tsx
@@ -1,5 +1,5 @@
-import MenuBarFileButton from "@/layouts/workbench-window/application-header/menu-bar/file-list/MenuBarFileButton";
-import MenuBarViewButton from "@/layouts/workbench-window/application-header/menu-bar/view-list/MenuBarViewButton";
+import MenuBarFileButton from "@/layouts/workbench-window/application-header/menu-bar/file-submenu/MenuBarFileButton";
+import MenuBarViewButton from "@/layouts/workbench-window/application-header/menu-bar/view-submenu/MenuBarViewButton";
 
 export default function MenuBar() {
   return (
