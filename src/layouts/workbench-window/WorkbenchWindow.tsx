ed1b21c128a2259d9f03661122ced97d0ca75aca
JFour404 <justas00001@gmail.com>
2026-02-17 00:40:33 +0200

    feat(ui): implement and integrate workbench tool bar

--- src/layouts/workbench-window/WorkbenchWindow.tsx
+++ src/layouts/workbench-window/WorkbenchWindow.tsx
@@ -1,9 +1,11 @@
 import ApplicationHeader from "./application-header/ApplicationHeader";
+import ToolBar from "./tool-bar/ToolBar";
 
 export default function WorkbenchWindow() {
   return (
     <div className="flex flex-col w-full h-full">
       <ApplicationHeader />
+      <ToolBar />
     </div>
   );
 }
---
+++ src/layouts/workbench-window/tool-bar/ToolBar.tsx
@@ -0,0 +1,36 @@
+import UiControlsButton from "@/features/ui-manager/components/tool-bar/UiControlsButton/UiControlsButton";
+import AddZoneButton from "@/features/canvas-editing/components/tool-bar/AddZoneButton/AddZoneButton";
+import AddObstacleButton from "@/features/canvas-editing/components/tool-bar/AddObstacleButton/AddObstacleButton";
+import DeleteButton from "@/features/canvas-editing/components/tool-bar/DeleteButton/DeleteButton";
+import ExecuteCppButton from "@/features/coverage-planning/components/ExecuteCppButton/ExecuteCppButton";
+import ClearCppButton from "@/features/coverage-planning/components/ClearCppButton/ClearCppButton";
+import OpenJobListButton from "@/features/job-runner/components/OpenJobListButton/OpenJobListButton";
+import UiInspectorButton from "@/features/ui-manager/components/tool-bar/UiInspectorButton/UiInspectorButton";
+import ToolSeparator from "@/components/ToolSeparator/ToolSeparator";
+
+export default function ToolBar() {
+  return (
+    <div className="flex items-center justify-between gap-3 px-3 py-1 bg-gray-100 border-b border-gray-300">
+      <div className="flex items-center gap-2">
+        <UiControlsButton />
+        <ToolSeparator />
+
+        <AddZoneButton />
+        <AddObstacleButton />
+
+        <ToolSeparator />
+        <DeleteButton />
+
+        <ToolSeparator />
+        <ExecuteCppButton />
+        <ClearCppButton />
+
+        <ToolSeparator />
+        <OpenJobListButton />
+      </div>
+      <div className="flex items-center gap-2">
+        <UiInspectorButton />
+      </div>
+    </div>
+  );
+}
