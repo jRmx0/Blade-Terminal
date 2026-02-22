e627cf39e8a92837dada69cc36c833848bb6cedf
JFour404 <justas00001@gmail.com>
2026-02-23 00:54:39 +0200

    refactor(controls-panel): inline algo, env, and object sections

--- src/layouts/workbench-window/controls-panel/ControlsPanel.tsx
+++ src/layouts/workbench-window/controls-panel/ControlsPanel.tsx
@@ -1,15 +1,30 @@
 import UiControlsPanel from "@/features/ui-manager/components/side-panels/UiControlsPanel";
-import AlgoSection from "./algo-section/AlgoSection";
-import EnvSection from "@/layouts/workbench-window/controls-panel/env-section/EnvSection";
-import ObjectSection from "@/layouts/workbench-window/controls-panel/object-section/ObjectSection";
+import ControlsPanelSection from "@/components/controls-panel/ControlsPanelSection";
+import AlgoSelect from "@/features/coverage-planning/components/controls-panel/algo-section/AlgoSelect";
+import PathWidthInput from "@/features/coverage-planning/components/controls-panel/algo-section/PathWidthInput";
+import PathOverlapInput from "@/features/coverage-planning/components/controls-panel/algo-section/PathOverlapInput";
+import GlobalTypeSelection from "@/features/coverage-planning/components/controls-panel/env-section/GlobalTypeSelect";
+import FormatSelection from "@/features/coverage-planning/components/controls-panel/env-section/FormatSelect";
+import ObjectTypeSelect from "@/features/coverage-planning/components/controls-panel/object-section/ObjectTypeSelect";
 
 export default function ControlsPanel() {
   return (
     <UiControlsPanel>
       <div>
-        <AlgoSection />
-        <EnvSection />
-        <ObjectSection />
+        <ControlsPanelSection sectionId="algo" title="Coverage Path">
+          <AlgoSelect />
+          <PathWidthInput />
+          <PathOverlapInput />
+        </ControlsPanelSection>
+
+        <ControlsPanelSection sectionId="env" title="Environment">
+          <GlobalTypeSelection />
+          <FormatSelection />
+        </ControlsPanelSection>
+
+        <ControlsPanelSection sectionId="object" title="Object">
+          <ObjectTypeSelect />
+        </ControlsPanelSection>
       </div>
     </UiControlsPanel>
   );
