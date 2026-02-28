51d02998750e95a710d77afb7799297846f51fc3
JFour404 <justas00001@gmail.com>
2026-02-28 14:54:22 +0200

    refactor(app-header): rename AppLogoBox to AppHeaderLogoBox

--- src/components/app-header/AppHeaderLogoBox.tsx
+++ src/components/app-header/AppHeaderLogoBox.tsx
@@ -1,6 +1,6 @@
 import btHexLogo from "@/assets/bt-hex.svg";
 
-export default function AppLogoBox() {
+export default function AppHeaderLogoBox() {
   return (
     <div className="w-12 h-12 p-0.5 bg-white-500 rounded-lg select-none">
       <img src={btHexLogo} alt="Blade Terminal" className="w-full h-full" />
