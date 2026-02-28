5672cadb6b98d51e7c152b18d0d90cfa326b6cc0
JFour404 <justas00001@gmail.com>
2026-02-28 13:22:53 +0200

    feat(status-bar): move SaveStateButton to workspace-manager

--- src/layouts/workbench-window/status-bar/StatusBar.tsx
+++ src/layouts/workbench-window/status-bar/StatusBar.tsx
@@ -1,5 +1,5 @@
 import UiStatusBar from "@/features/ui-manager/components/status-bar/UiStatusBar";
-import SaveStateButton from "@/features/ui-manager/components/status-bar/SaveStateButton";
+import SaveStateButton from "@/features/workspace-manager/components/status-bar/SaveStateButton";
 
 export default function StatusBar() {
   return <UiStatusBar leftChildren={<SaveStateButton />} />;
