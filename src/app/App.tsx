ce00b59f0f60557d753f07532a1a1e5df1f3421f
JFour404 <justas00001@gmail.com>
2026-03-03 20:03:48 +0200

    feat(shortcut-manager): move ShortcutManager component to shortcut-manager folder

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import WorkbenchWindow from "@/layouts/workbench-window/WorkbenchWindow";
-import ShortcutManager from "@/components/ShortcutManager/ShortcutManager";
+import ShortcutManager from "@/components/shortcut-manager/ShortcutManager";
 import { useEnvStore } from "@/stores/envStore";
 import { useBeforeUnload } from "@/hooks/useBeforeUnload";
 
