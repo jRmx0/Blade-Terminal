f28160ed88f6119b915e739c2ab7f8e2e2fb7e2a
JFour404 <justas00001@gmail.com>
2026-04-12 22:55:32 +0300

    feat(menu-bar): add performance monitor button

--- src/layouts/workbench-window/application-header/menu-bar/run-submenu/MenuBarRunSubmenu.tsx
+++ src/layouts/workbench-window/application-header/menu-bar/run-submenu/MenuBarRunSubmenu.tsx
@@ -1,10 +1,12 @@
 import ManageProvidersButton from "./ManageProvidersButton";
+import PerformanceMonitorButton from "./PerformanceMonitorButton";
 import MenuBarSubmenu from "@/components/menu-bar/MenuBarSubmenu";
 
 export default function MenuBarRunSubmenu() {
     return (
         <MenuBarSubmenu>
             <ManageProvidersButton />
+            <PerformanceMonitorButton />
         </MenuBarSubmenu>
     );
 }
---
+++ src/layouts/workbench-window/application-header/menu-bar/run-submenu/PerformanceMonitorButton.tsx
@@ -0,0 +1,5 @@
+import MenuBarItem from "@/components/menu-bar/MenuBarItem";
+
+export default function PerformanceMonitorButton() {
+    return <MenuBarItem label="Performance Monitor..." />;
+}
