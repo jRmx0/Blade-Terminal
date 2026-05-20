fbbfc19e7a1654ad2d25de433cf2b6db4f181d48
JFour404 <justas00001@gmail.com>
2026-05-20 05:35:51 +0300

    fix: update series prop to data in BenchmarkRunTab component

--- src/features/benchmark-manager/components/BenchmarkRunTab.tsx
+++ src/features/benchmark-manager/components/BenchmarkRunTab.tsx
@@ -384,7 +384,7 @@ export default function BenchmarkRunTab({
                                 key={series.metric}
                                 metricId={series.metricId}
                                 name={series.name}
-                                series={[{ label: series.name, data: series.data }]}
+                                data={series.data}
                                 xAxisLabel={targetParameterName}
                                 yAxisLabel={series.name.split(" vs ")[0]}
                             />
